aa368e0ed1860fc8b70da0c89433322bc4f4b8f1
Clement Ochieng <oclemmi@gmail.com>
2026-02-22 16:54:22 +0300

    Change default port from 5000 to 8080

--- app.py
+++ app.py
@@ -162,5 +162,5 @@ def websocket(ws):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
